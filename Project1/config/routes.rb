fd6446d222478ddcec840a741d9690cae7cbd698
Joshua <jpb1492@gmail.com>
2023-03-30 18:08:49 -0400

    Reimbursement

--- Project1/config/routes.rb
+++ Project1/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'reimbursement/show'
+  get 'reimbursement/create'
+  get 'reimbursement/destory'
+  get 'reimbursement/update'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
