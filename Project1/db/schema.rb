f489f086585b7b337c1006f997c94a91ed11a298
Arie <127146558+brodrigo1111@users.noreply.github.com>
2023-04-18 14:23:34 -0400

    final changes

--- Project1/db/schema.rb
+++ Project1/db/schema.rb
@@ -11,11 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_12_183744) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reimbursements", force: :cascade do |t|
     t.integer "amount", null: false
     t.string "description", null: false
     t.integer "status", default: 1
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_reimbursements_on_user_id"
